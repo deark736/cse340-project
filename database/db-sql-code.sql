c66f8684bde508234da45de8402345f8b429da1a
deark736 <dearken618@gmail.com>
2025-05-18 21:35:42 +0800

    Complete Assignment 2 SQL files (CRUD + rebuild)

--- database/db-sql-code.sql
+++ database/db-sql-code.sql
@@ -230,3 +230,13 @@ VALUES (
         'White',
         5
     );
+UPDATE public.inventory
+SET inv_description = REPLACE(
+        inv_description,
+        'small interiors',
+        'a huge interior'
+    )
+WHERE inv_model ILIKE '%hummer%';
+UPDATE public.inventory
+SET inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/'),
+    inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles/');
